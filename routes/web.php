2c72d7f1691b68a89b730104fce2785f80f47c30
takahashi kazunari <kamenaris@gmail.com>
2022-08-07 14:36:35 +0900

    データベースを作成、テーブルを作成、ダミーデータを挿入、全件取得して表示

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'shawId']);
+
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
