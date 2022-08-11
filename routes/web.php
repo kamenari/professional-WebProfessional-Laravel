e5520554f1a1de18ca4ad33179992fd05931fd0f
takahashi kazunari <kamenaris@gmail.com>
2022-08-11 21:51:42 +0900

    つぶやきの投稿機能を追加、バリデーションを追加、一覧取得を降順にする

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'shawId']);
 
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class) ->name('tweet.index');
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class) ->name('tweet.create');
