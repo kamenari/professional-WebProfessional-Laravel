6eb1418b7c3a6c44ee4d3ad9414f1e2f6d7437bb
takahashi kazunari <kamenaris@gmail.com>
2022-08-12 15:44:02 +0900

    削除機能を追加、つぶやき一覧の取得をサービスに切り出す

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tweet;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -14,10 +14,12 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         // $tweets = Tweet::all();
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get(); //降順
+        // $tweets = Tweet::orderBy('created_at', 'DESC')->get(); //降順
+        // $tweetService = new TweetService(); // TweetServiceのインスタンスを作成
+        $tweets = $tweetService->getTweets(); // TweetServiceインスタンスからgetTweets()を実行してつぶやき一覧を取得
         // dd($tweets);
         return view('tweet.index')
             ->with('tweets', $tweets);
